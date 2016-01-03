46bb84128251a1b10ad8752e2af52a26f0d93337
P. Mark Anderson <manderson@flightstats.com>
2016-01-02 23:34:52 -0800

    blink neopixels

--- martian-lighting-1.ino
+++ martian-lighting-1.ino
@@ -1,52 +1,74 @@
-//
-// see https://www.hackster.io/AgustinP/logging-temperature-data-using-the-spark-core-b9c60c
-// 
 
 #include "application.h"
-#include "HttpClient.h"
-#include "OneWire.h"
-#include "spark-dallas-temperature.h"
- 
-#define ONE_WIRE_BUS D2
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensor(&oneWire);
- 
-float temperature = 0.0;
-char resultstr[64];
-HttpClient http;
- 
-// ubidots sensor dashboard support
-// http://ubidots.com/
-#define VARIABLE_ID "56871c587625421c3844d644"
-#define TOKEN "u80i5jxgrvdRij5rKydxi2qbCinSY8LapYAluptBVb6BBC09b7KZNvB3Qpjn"
- 
-http_header_t headers[] = {
-      { "Content-Type", "application/json" },
-      { "X-Auth-Token" , TOKEN },
-    { NULL, NULL } // NOTE: Always terminate headers with NULL
-};
- 
-http_request_t request;
-http_response_t response;
+#include "neopixel.h"
+
+bool blink = true;
+bool blinkOff = false;
+uint32_t lastBlinkTimestamp = 0;
+
+// neopixel
+#define PIXEL_PIN D6
+#define PIXEL_COUNT 10
+#define PIXEL_TYPE WS2812B
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
+
  
 void setup() {
-    request.port = 80;
-    request.hostname = "things.ubidots.com";
-    request.path = "/api/v1.6/variables/" VARIABLE_ID "/values";
-    Serial.begin(9600);
-    sensor.begin();
+	strip.begin();
+	strip.show();
+    // Serial.begin(9600);
 }
  
 void loop() {
- sensor.requestTemperatures();
- temperature = sensor.getTempFByIndex( 0 );
-
- if (temperature > 0 && temperature < 115) {
- 	sprintf(resultstr, "{\"value\":%.4f}",temperature); 
- 	request.body = resultstr;//Sending presence to Ubidots
- 	http.post(request, response, headers);
- 	Serial.println(response.status); //For debug only
- 	Serial.println(response.body); 	
- }
- delay(5000);
+	blink = true;
+	rainbow(40);
 }
+
+// neopixel
+void rainbow(uint8_t wait) {
+	uint16_t i, j;
+
+	for(j=0; j<256; j++) {
+		float scale = blinkScale();
+		for(i=0; i<strip.numPixels(); i++) {
+			strip.setPixelColor(i, uint32_t(scale * Wheel((i+j) & 255)));
+		}
+		strip.show();
+		delay(wait);
+	}
+}
+
+float blinkScale() {
+	float scale = (blinkOff ? 1.0 : 0.0);
+	unsigned long now = millis();
+
+	if (lastBlinkTimestamp == 0) {
+		lastBlinkTimestamp = now;
+	}
+
+	if (blink) {
+		if (now - lastBlinkTimestamp > 3000) {
+			blinkOff = !blinkOff;
+			lastBlinkTimestamp = now;
+		}		
+	}
+
+	return scale;
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+	if(WheelPos < 85) {
+		return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+	} else if(WheelPos < 170) {
+		WheelPos -= 85;
+		return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+	} else {
+		WheelPos -= 170;
+		return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+	}
+}
+
+// end neopixel
